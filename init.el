f060047dc083cffe9c6032746b379feceb95c01d
re5et <re5etsmyth@gmail.com>
2010-04-16 18:30:50 -0400

    using custom.el for customize generated stuff

--- init.el
+++ init.el
@@ -73,32 +73,6 @@
 (global-font-lock-mode t t)
 (setq font-lock-maximum-decoration t)
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(css-indent-level 4)
- '(display-time-mode t)
- '(elscreen-display-tab nil)
- '(fringe-mode nil nil (fringe))
- '(global-linum-mode t)
- '(highline-selected-window t)
- '(highline-vertical nil)
- '(indicate-buffer-boundaries (quote left))
- '(indicate-empty-lines t)
- '(linum-delay t)
- '(linum-eager nil)
- '(mumamo-chunk-coloring 10)
- '(nxml-child-indent 4)
- '(nxml-outline-child-indent 4)
- '(save-place t nil (saveplace))
- '(sgml-basic-offset 4)
- '(size-indication-mode t)
- '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
- '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
-
 ;;; This was installed by package-install.el.
 ;;; This provides support for the package system and
 ;;; interfacing with ELPA, the package archive.
@@ -108,14 +82,6 @@
     (load
      (expand-file-name "~/.emacs.d/elpa/package.el"))
   (package-initialize))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :foreground "#00ff00" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 74 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
- '(fringe ((t (:background "#111" :foreground "#ff0"))))
- '(highline-face ((t (:background "#003"))))
- '(highline-vertical-face ((t (:background "#111"))))
- '(linum ((t (:inherit (shadow default) :foreground "#f60" :underline "#111"))))
- '(magit-item-highlight ((((class color) (background dark)) (:background "#100")))))
+
+setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)

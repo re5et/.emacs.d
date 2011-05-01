f887dc984c56ee7d68773ebf98167825f970b1c6
re5et <re5etsmyth@gmail.com>
2011-04-30 18:32:25 -0700

    bunch of files in mine / packages

---
+++ elpa/find-file-in-git-repo-0.1.2/find-file-in-git-repo-autoloads.el
@@ -0,0 +1,18 @@
+;;; find-file-in-git-repo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("find-file-in-git-repo-pkg.el" "find-file-in-git-repo.el")
+;;;;;;  (19900 43856 848270))
+
+;;;***
+
+(provide 'find-file-in-git-repo-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; find-file-in-git-repo-autoloads.el ends here
---
+++ elpa/smex-1.1.1/smex-autoloads.el
@@ -0,0 +1,27 @@
+;;; smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (smex-initialize) "smex" "smex.el" (19897 5227))
+;;; Generated autoloads from smex.el
+
+(autoload 'smex-initialize "smex" "\
+Not documented
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("smex-pkg.el") (19897 5227 887028))
+
+;;;***
+
+(provide 'smex-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smex-autoloads.el ends here

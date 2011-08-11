95dca78336cbd5f58afe0ba70036fdf010e18e0e
re5et <re5etsmyth@gmail.com>
2011-08-10 19:38:04 -0700

    download template update

--- emux-templates/downloads.el
+++ emux-templates/downloads.el
@@ -2,7 +2,4 @@
 (emux-create-screen nil "download-dir" "lm")
 (emux-hsplit "sb" "ssh sb")
 (emux-terminal-command "ll d/ --sort time -r")
-(emux-vsplit "scp")
-(emux-send-to-terminal "scp -r sb:d/")
-
-default-directory
+(emux-vsplit "rsync" "download_sync")

e643a6e909aba0146518cc34ccc1422afbfc2d43
re5et <re5etsmyth@gmail.com>
2011-10-09 20:25:28 -0700

    use built in kill whole line, hungry is buggy

--- mine/keybindings.el
+++ mine/keybindings.el
@@ -26,7 +26,7 @@
 (global-set-key (kbd "C-k") 'kill-line)
 (global-set-key (kbd "C-S-k") 'hungry-kill)
 (global-set-key (kbd "C-c k") 'copy-line)
-(global-set-key (kbd "C-c C-k") 'hungry-kill-whole-line)
+(global-set-key (kbd "C-c C-k") 'kill-whole-line)
 (global-set-key (kbd "C-<backspace>") 'backwards-kill)
 (global-set-key (kbd "S-<backspace>") 'pop-to-mark-command)
 (global-set-key (kbd "C-z") 'jump-to-string)

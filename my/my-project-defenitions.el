2f03a6c07bc89cfa5476f33528b91f715314e901
re5et <re5etsmyth@gmail.com>
2012-03-15 15:25:56 -0700

    add private dir to simp ignored

--- my/my-project-defenitions.el
+++ my/my-project-defenitions.el
@@ -1,9 +1,9 @@
-(project-define
+(simp-project-define
  '(:type rails
    :has (config.ru app/views app/models app/controllers)
-   :ignore (tmp coverage log vendor .git public/system public/assets)))
+   :ignore (tmp coverage log vendor .git private public/system public/assets)))
 
-(project-define
+(simp-project-define
  '(:type emacs
    :has (init.el)))
 

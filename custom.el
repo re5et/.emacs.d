6bb67848fea9fcc2261a42f80955756e244abaab
re5et <re5etsmyth@gmail.com>
2012-09-18 22:24:27 -0700

    Default magit branching does not include remote

--- custom.el
+++ custom.el
@@ -53,6 +53,7 @@
  '(linum-eager t)
  '(magit-commit-all-when-nothing-staged (quote ask-stage))
  '(magit-completing-read-function (quote ido-completing-read))
+ '(magit-default-tracking-name-function (quote magit-default-tracking-name-branch-only))
  '(magit-process-connection-type nil)
  '(magit-process-popup-time 0)
  '(magit-save-some-buffers nil)

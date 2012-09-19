d60494b916bca8e5f36e6417fc8c4134a7cbca90
re5et <re5etsmyth@gmail.com>
2012-09-18 23:08:16 -0700

    Remove stuff I don't think I need

--- re5et-terminal-theme.el
+++
@@ -1,9 +0,0 @@
-(deftheme re5et-terminal
-  "Created 2011-05-29.")
-
-(custom-theme-set-faces
- 're5et-terminal
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
- '(cursor ((t (:background "#0F0")))))
-
-(provide-theme 're5et-terminal)

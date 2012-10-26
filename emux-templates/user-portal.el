badeb1520e6bb84ef0159cb2f8c1b73f63c2e8d9
re5et <re5etsmyth@gmail.com>
2012-10-26 13:40:30 -0700

    Add emux template for user portal

---
+++ emux-templates/user-portal.el
@@ -0,0 +1,3 @@
+(emux-session-set-default-directory "~/code/user-portal")
+(emux-screen-create '(:name "guard") "guard" "be guard")
+(emux-screen-create '(:name "tail") "tail" "tail -f log/development.log")

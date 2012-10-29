45450d9b19b7f70c7944b7d86a8e90d609b6bce1
re5et <re5etsmyth@gmail.com>
2012-10-29 00:34:10 -0700

    Tune up project definitions a bit

--- my/my-project-defenitions.el
+++ my/my-project-defenitions.el
@@ -1,24 +1,42 @@
 (simp-project-define
  '(:type git
-   :has (.git)
-   :ignore (.git)))
+         :has (.git)
+         :ignore (.git)))
 
-(simp-project-define
- '(:type rails
-   :has (config.ru app/views app/models app/controllers)
-   :ignore (tmp coverage log vendor .git .css private public)))
+(let
+    ((rails-ignore '(tmp
+                     coverage
+                     log
+                     vendor
+                     .git
+                     .css
+                     private
+                     public
+                     .jhw-cache)))
 
-(simp-project-define
- '(:type node
-   :has (node_modules)))
+  (simp-project-define
+   `(:type rails
+           :has (config.ru app/views app/models app/controllers)
+           :ignore ,rails-ignore))
 
-(simp-project-define
- '(:type thoughtstream
-   :has (node-app rails-app npTuioClient)
-   :ignore (db docs .git ios-app npTuioClient scripts shared rails-app/public/javascripts node-app/node_modules shared .jhw-cache rails-app/tmp rails-app/public)))
+  (simp-project-define
+   '(:type node
+           :has (node_modules)))
 
-(simp-project-define
- '(:type emacs
-   :has (init.el)))
+  (simp-project-define
+   `(:type thoughtstream
+           :has (npTuioClient)
+           :ignore ,(append
+                     (mapcar (lambda (x) (format "rails-app/%s" x)) rails-ignore)
+                     '(db docs .git ios-app npTuioClient scripts shared node-app/node_modules shared))))
+
+  (simp-project-define
+   '(:type emacs
+           :has (init.el)))
+
+  (simp-project-define
+   '(:type git
+           :has (.git)
+           :ignore (.git))))
 
 (provide 'my-project-defenitions)

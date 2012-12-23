f0972ae983ba54059de9ea40734a67fc59323caf
re5et <re5etsmyth@gmail.com>
2012-12-23 01:33:45 -0800

    Magit commit advice should fix stuff

--- my/my-advice.el
+++ my/my-advice.el
@@ -1,14 +1,12 @@
 (defadvice magit-log-edit-commit (around magit-commit-babysitter)
   "Make sure we have a nice commit message."
-  (let ((ok-to-commit nil)
-        (bad-commit-message nil)
+  (let ((ok-to-commit t)
+        (commit-problems nil)
         (case-fold-search nil)
         (special-case (string-match "-- End of Magit header --" (buffer-string))))
     (save-excursion
       (beginning-of-buffer)
-      (print "special case:" special-case)
       (if special-case (goto-line 3))
-      (if special-case (print "penis"))
       (unless (string-match "[A-Z]" (string (char-after (point-min))))
         (if (yes-or-no-p "Doesn't start with a capital letter, fix?")
             (progn
@@ -16,22 +14,28 @@
               (capitalize-word 1))))
       (end-of-line)
       (if (> (current-column) 50)
-          (setq bad-commit-message "First line is too long (> 50 characters)."))
-      (next-line)
-      (if (and
-           (= (line-number-at-pos) 2)
-           (not (equal (point-at-bol) (point-at-eol)))
-           (setq bad-commit-message "The second line is not blank and there are more than one line.")))
-      (while (not (equal (point) (point-max)))
-        (forward-line)
-        (end-of-line)
-        (if (> (current-column) 72)
-            (setq bad-commit-message "There are lines that are too long (> 72 characters)")))
-      (if bad-commit-message
-          (if (yes-or-no-p (concat "WARNING: " bad-commit-message " still commit?"))
-              (setq ok-to-commit t))
+          (add-to-list 'commit-problems "First line is too long (> 50 characters)."))
+      (if (> (count-lines (point) (point-max)) 0)
+          (progn
+            (forward-line)
+            (if (not (equal (point-at-bol) (point-at-eol)))
+                (if (yes-or-no-p "Doesn't have a blank line after the first, fix?")
+                    (progn
+                      (beginning-of-line)
+                      (newline))))
+            (while (not (equal (point) (point-max)))
+              (forward-line)
+              (end-of-line)
+              (if (> (current-column) 72)
+                  (add-to-list 'commit-problems "There are lines that are too long (> 72 characters)")))))
+      (if commit-problems
+          (catch 'break
+            (dolist (problem commit-problems)
+              (unless (yes-or-no-p (concat "WARNING: " problem " still commit?"))
+                (progn
+                  (setq ok-to-commit nil)
+                  (throw 'break nil)))))
         (setq ok-to-commit t)))
-    (print "here" ok-to-commit)
     (if ok-to-commit ad-do-it)))
 
 (ad-activate 'magit-log-edit-commit)

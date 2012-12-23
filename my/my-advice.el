7873a132ec9427a3d108c182a5e77a2bab7a4783
re5et <re5etsmyth@gmail.com>
2012-12-22 22:29:25 -0800

    Update magit advice to not be annoying

--- my/my-advice.el
+++ my/my-advice.el
@@ -5,23 +5,25 @@
     (save-excursion
       (beginning-of-buffer)
       (unless (string-match "[A-Z]" (string (char-after (point-min))))
-        (setq bad-commit-message "Commit message should begin with a capital letter."))
+        (setq bad-commit-message "Commit message does not begin with a capital letter."))
       (end-of-line)
       (if (> (current-column) 50)
-          (setq bad-commit-message "First line is too long, keep it to under 50"))
+          (setq bad-commit-message "First line is too long (> 50 characters)."))
       (goto-line 2)
       (if (and
+           (not (string-match "-- End of Magit header --") (buffer-string))
            (= (line-number-at-pos) 2)
            (not (equal (point-at-bol) (point-at-eol)))
-           (setq bad-commit-message "The second line should be blank if there is more than one line.")))
+           (setq bad-commit-message "The second line is not blank and there are more than one line.")))
       (while (not (equal (point) (point-max)))
         (forward-line)
         (end-of-line)
         (if (> (current-column) 72)
-            (setq bad-commit-message "You have lines that are too long, keep them under 72.")))
+            (setq bad-commit-message "There are lines that are too long (> 72 characters)")))
       (if bad-commit-message
-          (message bad-commit-message)
-        ad-do-it))))
+          (if (yes-or-no-p (concat "WARNING: " bad-commit-message " still commit?"))
+              ad-do-it)))))
+
 (ad-activate 'magit-log-edit-commit)
 
 (provide 'my-advice)

8621e49d4a34d4dc6245369da5dce76953bddb59
re5et <re5etsmyth@gmail.com>
2012-12-27 17:45:01 -0800

    Add rust mode

---
+++ elpa/cm-mode-20120814.1203/cm-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; cm-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (cm-mode) "cm-mode" "cm-mode.el" (20700 50869))
+;;; Generated autoloads from cm-mode.el
+
+(autoload 'cm-mode "cm-mode" "\
+
+
+\(fn MODE)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("cm-mode-pkg.el") (20700 50869 936533))
+
+;;;***
+
+(provide 'cm-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cm-mode-autoloads.el ends here

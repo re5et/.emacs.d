e4406dfa6b2d3902191a4cf5b732422247b9f68a
re5et <re5etsmyth@gmail.com>
2012-12-29 01:57:07 -0800

    Install itail from melpa

---
+++ elpa/itail-20121227.1309/itail-autoloads.el
@@ -0,0 +1,18 @@
+;;; itail-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("itail-pkg.el" "itail.el") (20702 225
+;;;;;;  844468))
+
+;;;***
+
+(provide 'itail-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; itail-autoloads.el ends here
---
+++ elpa/itail-20121227.1309/itail-pkg.el
@@ -0,0 +1,1 @@
+(define-package "itail" "20121227.1309" "An interactive tail mode [source: github]" (quote nil))

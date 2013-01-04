30ee6ad72c7eccc33dc40854141f305d649dfb17
Adam Smith <re5etsmyth@gmail.com>
2013-01-03 16:23:28 -0800

    No guard notifications

--- emux-templates/user-portal.el
+++ emux-templates/user-portal.el
@@ -1,4 +1,4 @@
 (emux-session-set-default-directory "~/code/user-portal")
-(emux-screen-create '(:name "guard") "guard" "be guard")
+(emux-screen-create '(:name "guard") "guard" "be guard --notify false")
 (emux-screen-create '(:name "tail" :no-terminal t))
 (itail "tail -f log/development.log")

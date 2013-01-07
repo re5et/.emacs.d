4c42cb3caf6d8ba6fb0814a5fafea746982bbaf1
re5et <re5etsmyth@gmail.com>
2013-01-06 18:29:57 -0800

    Smylink simp

--- packages/simp-0.2.0/simp-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; simp-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("simp-pkg.el" "simp-project-files.el"
-;;;;;;  "simp-project-rgrep.el" "simp-project.el" "simp.el") (20641
-;;;;;;  50489 124279))
-
-;;;***
-
-(provide 'simp-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; simp-autoloads.el ends here

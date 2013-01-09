5c1d4de484249a7e233805b86874c3025801df9a
Adam Smith <re5etsmyth@gmail.com>
2013-01-09 12:49:32 -0800

    Update packages

---
+++ packages/magit-20130108.758/magit-pkg.el
@@ -0,0 +1,2 @@
+(define-package "magit" "20130108.758" "Control Git from Emacs. [source: github]" 'nil)
+
---
+++ packages/slime-20130109.1429/slime-pkg.el
@@ -0,0 +1,2 @@
+(define-package "slime" "20130109.1429" "Superior Lisp Interaction Mode for Emacs [source: github]" 'nil)
+

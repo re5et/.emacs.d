e82b24eb373fe5dbdb164b408e1f3d1f5af16cd9
re5et <re5etsmyth@gmail.com>
2013-02-03 14:43:09 -0800

    Update packages

---
+++ packages/multiple-cursors-20130130.1634/multiple-cursors-pkg.el
@@ -0,0 +1,2 @@
+(define-package "multiple-cursors" "20130130.1634" "Multiple cursors for Emacs. [source: github]" 'nil)
+
---
+++ packages/slime-20130203.1213/slime-pkg.el
@@ -0,0 +1,2 @@
+(define-package "slime" "20130203.1213" "Superior Lisp Interaction Mode for Emacs [source: github]" 'nil)
+

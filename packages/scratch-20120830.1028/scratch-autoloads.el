f7ee81245cd26f4ea50838d2e528b25ece471d45
Adam Smith <re5etsmyth@gmail.com>
2013-03-28 13:00:23 -0700

    Add scratch package

---
+++ packages/scratch-20120830.1028/scratch-autoloads.el
@@ -0,0 +1,28 @@
+;;; scratch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (scratch) "scratch" "scratch.el" (20820 41235))
+;;; Generated autoloads from scratch.el
+
+(autoload 'scratch "scratch" "\
+Get a scratch buffer for the current mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("el-get.el" "scratch-pkg.el") (20820 41235
+;;;;;;  527945))
+
+;;;***
+
+(provide 'scratch-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; scratch-autoloads.el ends here

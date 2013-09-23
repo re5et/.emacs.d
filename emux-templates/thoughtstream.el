d6c81b5297a0ccda49abb0964bcf5bd2b1660d10
re5et <re5etsmyth@gmail.com>
2013-09-23 13:51:42 -0700

    Open consols

--- emux-templates/thoughtstream.el
+++ emux-templates/thoughtstream.el
@@ -1,5 +1,7 @@
 (emux-session-set-default-directory "/vagrant:")
 (emux-screen-create '(:name "misc") "vagrant")
+(emux-screen-create '(:name "rcs") "user_management-rc" "cd user_management && bundle exec rails c")
+(emux-term-vsplit "browser_client-rc" "cd browser_client && bundle exec rails c")
 (emux-screen-create '(:name "guards"))
 (emux-term-command "./browser_client_guard")
 (emux-term-vsplit "collaboration_service" "./collaboration_service_guard")

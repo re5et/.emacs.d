14c8289811133065bb5ad5f50e77840bea5f979e
re5et <re5etsmyth@gmail.com>
2015-05-21 00:40:10 -0700

    Switch to latest melpa

--- my/my-package.el
+++ my/my-package.el
@@ -1,7 +1,6 @@
 (require 'package)
 
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;; (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
 
 (provide 'my-package)

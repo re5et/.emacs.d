f1a330b5e3bd4448094e72785a9552a5ccb94cbb
re5et <re5etsmyth@gmail.com>
2016-01-19 10:31:23 -0800

    Add jsx mode

--- my/my-add-to-lists.el
+++ my/my-add-to-lists.el
@@ -7,5 +7,6 @@
 (add-to-list 'auto-mode-alist '("\\.stumpwmrc'" . stumpwm-mode))
 (add-to-list 'auto-mode-alist '("\\.hamlbars'" . haml-mode))
 (add-to-list 'auto-mode-alist '("\\.hamlc'" . haml-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 (provide 'my-add-to-lists)

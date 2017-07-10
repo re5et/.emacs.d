770ce34b72163c5b685b73445f37bfd3d5f69c4d
re5et <re5etsmyth@gmail.com>
2017-07-10 10:31:45 -0700

    package updates

---
+++ packages/request-20170131.1747/request-autoloads.el
@@ -0,0 +1,15 @@
+;;; request-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("request.el") (22802 7293 886529 806000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; request-autoloads.el ends here
